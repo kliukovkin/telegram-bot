b072270968beefbc8d4c41338a50c5806e72fdf1
Georgy Klyukovkin <georgy.klyukovkin@nordigy.ru>
2019-01-07 11:37:33 +0300

    weather api prepared

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 import Telegraf from 'telegraf';
 import config from 'config';
 import HttpsProxyAgent from 'https-proxy-agent';
+import request from 'request';
 
 const TOKEN = config.get('token');
-const proxy = config.get('proxy');
+const PROXY = config.get('proxy');
+const APIKEY = config.get('apiKey');
+console.log(APIKEY);
+let city = 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`;
 
 const bot = new Telegraf(TOKEN, {
     telegram: {
         agent: new HttpsProxyAgent({
-            host: proxy.host,
-            port: proxy.port
+            host: PROXY.host,
+            port: PROXY.port
         })
     },
 });
+request(url, function (err, response, body) {
+    if(err){
+        console.log('error:', error);
+    } else {
+        console.log('body:', body);
+    }
+});
 
-bot.hears('hi', ctx => {
-    return ctx.reply('Hey!');
+bot.on('message', ctx => {
+    return ctx.reply('yo!')
 });
 bot.startPolling();
